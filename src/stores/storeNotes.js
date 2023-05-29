3224f9f9ed571e17ce3bcd71d7b8be2196c1c702
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-05-29 16:55:11 +0200

    getting notes array from Pinia

---
+++ src/stores/storeNotes.js
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+
+export const useStoreNotes = defineStore("storeNotes", {
+  state: () => {
+    return {
+      notes: [
+        {
+          id: "id1",
+          content:
+            "sed est error fugit possimus etsed est error fugit possimus etsed est error fugit possimus et",
+        },
+        {
+          id: "id2",
+          content:
+            "Ipsum molestiae possimus saepe laboriosam quis. Ex labore eaque dolor sequi laboriosam ab aut distinctio ea. Laborum ut ut nihil rerum.",
+        },
+        {
+          id: "id3",
+          content:
+            "Voluptatem qui dolorem nam ut culpa ea ipsum ipsa nobis quisquam enim ut. In ducimus eveniet accusantium. Ex nulla sunt voluptas. Mollitia dolor minus perferendis rerum illo autem libero recusandae laborum aspernatur modi. Voluptatem impedit sint quo. Alias mollitia et id ea et ea doloribus molestiae ut ducimus voluptas velit eos inventore. Aliquid reiciendis omnis rerum nam suscipit expedita aliquam illo. Possimus odio reprehenderit itaque corrupti molestias ut non a consequatur excepturi perspiciatis.",
+        },
+      ],
+    };
+  },
+});
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -24,7 +24,7 @@
     </div>
 
     <Note
-      v-for="note in notes"
+      v-for="note in storeNotes.notes"
       key="note.id"
       :note="note"
       @deleteClicked="deleteNote" />
@@ -35,28 +35,14 @@
 /* Imports */
 import { ref } from "vue";
 import Note from "@/components/notes/Note.vue";
+import { useStoreNotes } from "@/stores/storeNotes.js";
 
 /* Notes */
 const newNote = ref("");
 const newNoteRef = ref(null);
 
-const notes = ref([
-  {
-    id: "id1",
-    content:
-      "sed est error fugit possimus etsed est error fugit possimus etsed est error fugit possimus et",
-  },
-  {
-    id: "id2",
-    content:
-      "Ipsum molestiae possimus saepe laboriosam quis. Ex labore eaque dolor sequi laboriosam ab aut distinctio ea. Laborum ut ut nihil rerum.",
-  },
-  {
-    id: "id3",
-    content:
-      "Voluptatem qui dolorem nam ut culpa ea ipsum ipsa nobis quisquam enim ut. In ducimus eveniet accusantium. Ex nulla sunt voluptas. Mollitia dolor minus perferendis rerum illo autem libero recusandae laborum aspernatur modi. Voluptatem impedit sint quo. Alias mollitia et id ea et ea doloribus molestiae ut ducimus voluptas velit eos inventore. Aliquid reiciendis omnis rerum nam suscipit expedita aliquam illo. Possimus odio reprehenderit itaque corrupti molestias ut non a consequatur excepturi perspiciatis.",
-  },
-]);
+/* Store */
+const storeNotes = useStoreNotes();
 
 const addNote = () => {
   let currentDate = new Date().getTime();
