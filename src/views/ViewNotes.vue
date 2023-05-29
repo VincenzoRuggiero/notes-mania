b08dd6092cc7a0df3abc9f2b2a2bf331d32b20f3
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-05-29 15:28:01 +0200

    Looping hard coded notes array

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -16,12 +16,9 @@
       </div>
     </div>
 
-    <div class="card mb-4">
+    <div class="card mb-4" v-for="note in notes" key="note.id">
       <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-          placeat saepe voluptatibus.
-        </div>
+        <div class="content">{{ note.content }}</div>
       </div>
       <footer class="card-footer">
         <a href="#" class="card-footer-item">Edit</a>
@@ -30,3 +27,27 @@
     </div>
   </div>
 </template>
+
+<script setup>
+/* Imports */
+import { ref } from "vue";
+
+/* Notes */
+const notes = ref([
+  {
+    id: "id1",
+    content:
+      "sed est error fugit possimus etsed est error fugit possimus etsed est error fugit possimus et",
+  },
+  {
+    id: "id2",
+    content:
+      "Ipsum molestiae possimus saepe laboriosam quis. Ex labore eaque dolor sequi laboriosam ab aut distinctio ea. Laborum ut ut nihil rerum.",
+  },
+  {
+    id: "id3",
+    content:
+      "Voluptatem qui dolorem nam ut culpa ea ipsum ipsa nobis quisquam enim ut. In ducimus eveniet accusantium. Ex nulla sunt voluptas. Mollitia dolor minus perferendis rerum illo autem libero recusandae laborum aspernatur modi. Voluptatem impedit sint quo. Alias mollitia et id ea et ea doloribus molestiae ut ducimus voluptas velit eos inventore. Aliquid reiciendis omnis rerum nam suscipit expedita aliquam illo. Possimus odio reprehenderit itaque corrupti molestias ut non a consequatur excepturi perspiciatis.",
+  },
+]);
+</script>
